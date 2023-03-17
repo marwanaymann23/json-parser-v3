bdab7c3d4a76b8f4a4b268fec41523362bae0697
marwanaymann <marwan.aymann23@gmail.com>
2023-03-17 19:52:19 +0200

    Take input in console

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,18 @@ COPY . /usr/src/myapp
 WORKDIR /usr/src/myapp/src
 RUN javac Main.java
 CMD ["java", "Main"]
+
+
+# Commands
+
+# In Intellij terminal
+# 1- docker build -t json-parser-task-v3 .
+# 2-  docker run --name json-parser-app-v3 --rm -i json-parser-task-v3
+
+# In Powershell
+# 1- docker login
+# 2- docker images and copy the name of image "json-parser-task-v3"
+# 3-  docker tag json-parser-task-v3 marwannayman/json-parser-task-v3
+# 4- docker push marwannayman/json-parser-task-v3
+
+
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        String jsonInput = "{\"key1\": \"value1\", \"key2\": \"value2\", \"key3\": \"value3\", \"key4\": \"value4\", \"key5\": \"value5\"}";
-
+        // Input
+        // {"key1": "value1", "key2": "value2", "key3": "value3", "key4": "value4", "key5": "value5", "key6": "value6", "key17": "value17"}
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the input in the following format -> \"{\"key1\": \"value1\"}");
+        String jsonInput = in.nextLine();
         try {
 
             ArrayList<String> keys = new ArrayList<>();
@@ -30,12 +33,15 @@ public class Main {
                 }
 
                 if(i == 0)
-                    System.out.print("First Key: " + keys.get(i) + ", Value" + ": " + values.get(i) + ",\t\t");
+                    System.out.print("First Key: " + keys.get(i).substring(0) + ", Value" + ": " + values.get(i) + ",\t\t");
                 else if(i == 1)
                     System.out.print("Second Key: " + keys.get(i) + ", Value" + ": " + values.get(i) + ",\t\t");
                 else if(i == 2)
                     System.out.print("Third Key: " + keys.get(i) + ", Value" + ": " + values.get(i) + ",\t\t");
-                else
+                else if (i == keys.size() - 1) {
+                    System.out.print(i+1 + "th Key: " + keys.get(i) + ", Value" + ": " + values.get(i).substring(0, values.get(i).length()));
+
+                } else
                     System.out.print(i+1 + "th Key: " + keys.get(i) + ", Value" + ": " + values.get(i) + ",\t\t");
 
                 spacer++;
